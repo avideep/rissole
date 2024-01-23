f2b50ec9aa0178782b0954f0bc70a4ef331c8f25
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-01-23 15:52:13 +0530

    up

--- train_ddpm_vae2.py
+++ train_ddpm_vae2.py
@@ -278,6 +278,7 @@ def validate(model, data_loader, block_size, vae, device):
             #     prev_block = model.encode(prev_block)
             block_pos = torch.full((n_images,),position, dtype=torch.int64).to(device)
             curr_block = model.sample(block_size, prev_block, block_pos, low_res_cond, batch_size=n_images, channels=latent_dim)
+            curr_block[0] = curr_block[0] - low_res_cond 
             prev_block = curr_block[0]
             position += 1
             for k in range(len(curr_block)):
