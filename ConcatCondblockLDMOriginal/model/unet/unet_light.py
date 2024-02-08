4966715745c97a8133a3728b4ce51ac59124c7c9
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-02-08 16:31:35 +0530

    update

--- ConcatCondblockLDMOriginal/model/unet/unet_light.py
+++ ConcatCondblockLDMOriginal/model/unet/unet_light.py
@@ -63,7 +63,7 @@ class UNetLight(nn.Module):
         # bottleneck
         self.mid_block1 = ResidualBlockUNet(self.channels[-1], self.channels[-1], time_emb_dim, cond_emb_dim, n_groups)
         # self.mid_attn = SpatialTransformer(self.channels[-1], n_heads, dim_keys, depth=1, context_dim= cond_emb_dim) if use_spatial_transformer else Attention(self.channels[-1], n_heads, dim_keys)
-        self.mid_attn = Attention(self.channels[-1], n_heads, dim_keys)
+        # self.mid_attn = Attention(self.channels[-1], n_heads, dim_keys)
 
  
         self.mid_block2 = ResidualBlockUNet(self.channels[-1], self.channels[-1], time_emb_dim, cond_emb_dim, n_groups)
@@ -157,7 +157,7 @@ class UNetLight(nn.Module):
         #     x = self.mid_attn(x,c)
         # else:
         #     x = self.mid_attn(x)
-        x = self.mid_attn(x)
+        # x = self.mid_attn(x)
         x = self.mid_block2(x, t, p)
 
         # up sample
