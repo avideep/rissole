f6198cd2fe08fce4eb17ceb15eba48d513ed372f
avideep <avideep@cse.iitk.ac.in>
2024-02-29 01:15:35 +0530

    up

--- RetrievalAugmentedBlockLDM/dataloader/celeba.py
+++ RetrievalAugmentedBlockLDM/dataloader/celeba.py
@@ -26,6 +26,7 @@ class CelebA:
         self.mean = [0.5, 0.5, 0.5]
         self.std = [0.5, 0.5, 0.5]
         self.patch_size = img_size // 4
+        self.patches = 5
         self.train_transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize(img_size),
@@ -40,12 +41,14 @@ class CelebA:
             transforms.CenterCrop(64),
             transforms.Normalize(self.mean, self.std)
         ])
-        IMAGE_PATH = '/data/avideep/blockLDM/data/celeba/processed/'
+        self.ROOT_PATH = '/hdd/avideep/blockLDM/data/celeba/'
+        self.IMAGE_PATH = '/hdd/avideep/blockLDM/data/celeba/processed/'
+        self.DSET_PATH = '/hdd/avideep/blockLDM/data/celeba/dset.pth'
         train_size = int(50000 * 0.9)
         # val size is 10000 in cifar10
         test_size = 50000 - train_size
 
-        self.train_set_full = ImageFolder(IMAGE_PATH, self.train_transform)
+        self.train_set_full = ImageFolder(self.IMAGE_PATH, self.train_transform)
         self.train_loader, self.val_loader = self.train_val_test_split(self.train_set_full, self.batch_size)
         self.full_dataloader = DataLoader(self.train_set_full, batch_size=1, num_workers=12, pin_memory=True)
         # invert normalization for tensor to image transform
@@ -54,6 +57,7 @@ class CelebA:
             transforms.Normalize(mean=[-m for m in self.mean], std=1.),
             lambda x: x*255
         ])
+        self.dset = self.dsetbuilder()
     def train_val_test_split(self, dataset, batch_size=16, test_ratio=0.2, val_ratio=0.2):
 
         np.random.seed(42)
@@ -90,6 +94,10 @@ class CelebA:
     def val(self):
         """ Return validation dataloader. """
         return self.val_loader
+    
+    @property
+    def dset(self):
+        return self.dset
 
     # @property
     # def test(self):
@@ -115,11 +123,30 @@ class CelebA:
             return Image.fromarray(img.permute(1, 2, 0).numpy().astype('uint8')).convert("RGB")
         else:
             return Image.fromarray(img[0].numpy()).convert("L")
+    def select_random_patches(self, image):
+        _, _, height, width = image.shape
+
+
+        patches = []
+
+        for _ in range(self.patches):
+            top_left_y = torch.randint(0, height - self.patch_size + 1, (1,))
+            top_left_x = torch.randint(0, width - self.patch_size + 1, (1,))
+
+            patch = image[:, :, top_left_y:(top_left_y + self.patch_size), top_left_x:(top_left_x + self.patch_size)]
+            patches.append(patch)
+
+        return patches
     def dsetbuilder(self):
         """ Creates the D Set for this particular Dataset"""
-        for x, _ in self.full_dataloader:
-            
-        pass
+        if os.path.exists(self.DSET_PATH):
+            all_patches = torch.load(self.DSET_PATH)
+        else:
+            all_patches = []
+            for x, _ in self.full_dataloader:
+                for patch in self.select_random_patches(x):
+                    all_patches.append(self.encoder.encode(self.tensor2img(patch)))
+        return all_patches
 
 class CelebAHQ:
     def __init__(self, batch_size: int = 16, img_size = 256):
@@ -150,7 +177,7 @@ class CelebAHQ:
             transforms.CenterCrop(256),
             transforms.Normalize(self.mean, self.std)
         ])
-        IMAGE_PATH = '/data/avideep/blockLDM/data/celeba/celeba_hq_256/'
+        IMAGE_PATH = '/hdd/avideep/blockLDM/data/celeba/celeba_hq_256/'
         train_size = int(30000 * 0.9)
         # val size is 10000 in cifar10
         test_size = 30000 - train_size
