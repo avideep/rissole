50ebdf3025114a37dbc33158832f84d6eec1b9c2
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-02-29 14:37:34 +0530

    update

--- RetrievalAugmentedBlockLDM/dataloader/cifar10.py
+++ RetrievalAugmentedBlockLDM/dataloader/cifar10.py
@@ -3,10 +3,13 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 from PIL import Image
-
+from torch.utils.data import DataLoader
+from sentence_transformers import SentenceTransformer, util
+from tqdm import tqdm
+import numpy as np
 
 class CIFAR10:
-    def __init__(self, batch_size: int = 16):
+    def __init__(self, batch_size: int = 16, img_size = 128):
         """
         Wrapper to load, preprocess and deprocess CIFAR-10 dataset.
         Args:
@@ -19,36 +22,40 @@ class CIFAR10:
 
         self.mean = [0.5, 0.5, 0.5]
         self.std = [0.5, 0.5, 0.5]
-
+        self.patch_size = img_size // 2
+        self.patches = 5
         self.train_transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Resize(128),
+            transforms.Resize(img_size),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.Normalize(self.mean, self.std)
         ])
+        self.encoder = SentenceTransformer('clip-ViT-B-32')
 
         self.val_transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize(128),
             transforms.Normalize(self.mean, self.std)
         ])
-
+        self.ROOT_PATH = '/hdd/avideep/blockLDM/data/cifar10/'
+        self.DSET_PATH = '/hdd/avideep/blockLDM/data/cifar10/dset.pth'
         train_size = int(50000 * 0.9)
         # val size is 10000 in cifar10
         test_size = 50000 - train_size
 
-        self.train_set_full = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
+        self.train_set_full = torchvision.datasets.CIFAR10(root=self.ROOT_PATH, train=True, download=True,
                                                            transform=self.train_transform)
         # TODO: check if this way of splitting in train, val and test is correct
         self.train_set, self.test_set = torch.utils.data.random_split(self.train_set_full, [train_size, test_size])
 
         self.train_loader = torch.utils.data.DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True)
 
-        self.val_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True,
+        self.val_set = torchvision.datasets.CIFAR10(root=self.ROOT_PATH, train=False, download=True,
                                                     transform=self.val_transform)
         self.val_loader = torch.utils.data.DataLoader(self.val_set, batch_size=self.batch_size, shuffle=False)
 
         self.test_loader = torch.utils.data.DataLoader(self.test_set, batch_size=self.batch_size, shuffle=False)
+        self.full_dataloader = DataLoader(self.train_set_full, batch_size=1, num_workers=12, pin_memory=True)
 
         # invert normalization for tensor to image transform
         self.inv_normalize = transforms.Compose([
@@ -56,6 +63,7 @@ class CIFAR10:
             transforms.Normalize(mean=[-m for m in self.mean], std=1.),
             lambda x: x*255
         ])
+        self.dset = self.dsetbuilder()
 
     @property
     def train(self):
@@ -91,6 +99,31 @@ class CIFAR10:
             return Image.fromarray(img.permute(1, 2, 0).numpy().astype('uint8')).convert("RGB")
         else:
             return Image.fromarray(img[0].numpy()).convert("L")
+    def select_random_patches(self, image):
+        _, _, height, width = image.shape
+
+
+        patches = []
+
+        for _ in range(self.patches):
+            top_left_y = torch.randint(0, height - self.patch_size + 1, (1,))
+            top_left_x = torch.randint(0, width - self.patch_size + 1, (1,))
+
+            patch = image[:, :, top_left_y:(top_left_y + self.patch_size), top_left_x:(top_left_x + self.patch_size)]
+            patches.append(torch.squeeze(patch, dim=0))
+
+        return patches
+    def dsetbuilder(self):
+        """ Creates the D Set for this particular Dataset"""
+        if os.path.exists(self.DSET_PATH):
+            all_patches = torch.load(self.DSET_PATH)
+        else:
+            all_patches = []
+            for x, _ in tqdm(self.full_dataloader, desc='Building DSET'):
+                for patch in self.select_random_patches(x):
+                    all_patches.append(self.encoder.encode(self.tensor2img(patch)))
+            torch.save(torch.tensor(np.array(all_patches)), self.DSET_PATH)
+        return all_patches
 
 
 if __name__ == "__main__":
