afbb8f24e5c01ca035c2239193825b99f21b2995
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-03-05 12:11:17 +0530

    up

--- RetrievalAugmentedBlockLDM/train_ddpm_vae2.py
+++ RetrievalAugmentedBlockLDM/train_ddpm_vae2.py
@@ -229,7 +229,7 @@ def train(model, data, optimizer, block_size, vae, device, args):
                 curr_block = x[:, :, i:i+block_size, j:j+block_size]
                 prev_block = model.decode(prev_block)
                 # print(prev_block.shape)
-                neighbors = data.get_neighbors(prev_block, block_size)
+                neighbors = data.get_neighbors(prev_block, block_size).to(device)
                 loss = model.p_losses2(curr_block, neighbors, position = block_pos, low_res_cond = low_res_cond)
                 prev_block = curr_block
                 loss_agg += loss
