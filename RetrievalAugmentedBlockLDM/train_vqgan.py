eabb39f3b7303d1d6f3ce3e1ed86c19323405857
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-03-23 12:36:07 +0530

    update

--- RetrievalAugmentedBlockLDM/train_vqgan.py
+++ RetrievalAugmentedBlockLDM/train_vqgan.py
@@ -93,7 +93,7 @@ def main():
     elif args.data == 'CIFAR10':
         data = CIFAR10(args.batch_size)
     elif args.data == 'ImageNet100':
-        data = ImageNet100(args.batch_size, dset_batch_size= args.dset_batch_size, device=device)
+        data = ImageNet100(args.batch_size, dset_batch_size= args.dset_batch_size)
     else:
         data = CelebAHQ(args.batch_size, dset_batch_size= args.dset_batch_size, device=device)
     # read config file for model
