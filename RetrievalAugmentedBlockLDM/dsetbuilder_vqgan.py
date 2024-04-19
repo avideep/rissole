0acfe78a33982aa75835342ebd6679ff44f33f4a
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-04-19 11:19:37 +0530

    sd

--- RetrievalAugmentedBlockLDM/dsetbuilder_vqgan.py
+++ RetrievalAugmentedBlockLDM/dsetbuilder_vqgan.py
@@ -243,4 +243,4 @@ if __name__ == "__main__":
         data = ImageNet100(batch_size = args.batch_size, dset_batch_size = args.dset_batch_size)
     else:
         data = CIFAR10(batch_size = args.batch_size, dset_batch_size = args.dset_batch_size)
-    dset = ClassDSetBuilder(data, k=20, model=vqgan_model, device=device)
+    dset = DSetBuilder(data, k=20, model=vqgan_model, device=device)
