397c0025bcf8b3eef79173ce52b8b095d5655124
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-04-19 11:27:16 +0530

    dfs

--- RetrievalAugmentedBlockLDM/dsetbuilder_vqgan.py
+++ RetrievalAugmentedBlockLDM/dsetbuilder_vqgan.py
@@ -110,7 +110,9 @@ class DSetBuilder:
                         patch = z[:, :, i:i+self.latent_patch_size, j:j+self.latent_patch_size]
                         patches.append(patch.cpu().detach())
                         del x, z, patch
-                    all_patches.append(torch.cat(patches, dim=0).view(len(self.data.full_dataloader.dataset), -1))
+                    # all_patches.append(torch.cat(patches, dim=0).view(len(self.data.full_dataloader.dataset), -1))
+                        cat_patch = torch.cat(patches, dim=0)
+                        print(cat_patch.shape)
             all_patches = torch.stack(all_patches)
             torch.save(all_patches, self.DSET_PATH)
         print('DSET with shape: {} is ready!'.format(all_patches.shape))
