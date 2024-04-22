39e5f00fc08164fd4472e2482adb75941421b5a0
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-04-22 13:28:59 +0530

    vc

--- RetrievalAugmentedBlockLDM/make_ddpm_evaluation_data.py
+++ RetrievalAugmentedBlockLDM/make_ddpm_evaluation_data.py
@@ -33,7 +33,7 @@ parser.add_argument('--block-factor', default=4, metavar='N',
                     type=int, help='Size of the block that the image will be divided by.')
 parser.add_argument('--k', default=20, metavar='N',
                     type=int, help='Number of nearest neighbors to search.')
-parser.add_argument('--image-count', default=10,
+parser.add_argument('--image-count', default=20,
                     type=int, help='number of images that should be generated for comparison')
 parser.add_argument('--config', default='configs/ddpm_linear.yaml',
                     metavar='PATH', help='Path to model config file (default: configs/ddpm_linear.yaml)')
