6ee8be5b2fa9b118e0ced520e780beda55ce1c48
Avideep Mukherjee <mukherjeeavideep@gmail.com>
2024-05-06 12:24:04 +0530

    dfsa

--- RetrievalAugmentedBlockLDM/make_ddpm_evaluation_data.py
+++ RetrievalAugmentedBlockLDM/make_ddpm_evaluation_data.py
@@ -77,8 +77,8 @@ def main():
         print("{:<16}: {}".format(name, val))
 
     if args.sample_real:
-        if args.real_image_path and not os.path.exists(args.real_image_path):
-            os.makedirs(args.real_image_path)
+        # if args.real_image_path and not os.path.exists(args.real_image_path):
+        #     os.makedirs(args.real_image_path)
         # GPU setup
         args.gpus = args.gpus if isinstance(args.gpus, list) else [args.gpus]
         if len(args.gpus) == 1:
@@ -89,15 +89,19 @@ def main():
 
         if args.data == 'CelebA':
             args.img_size = 64
+            args.real_image_path += 'celeba/'
             data = CelebA(root= args.data_path, batch_size= args.batch_size)
         elif args.data == 'CIFAR10':
             data = CIFAR10(args.batch_size)
         elif args.data == 'ImageNet100':
             args.img_size = 224
             data = ImageNet100(root= args.data_path, batch_size = args.batch_size, dset_batch_size = args.dset_batch_size)
+            args.real_image_path += 'imagenet100/'
         else:
             data = CelebAHQ(args.batch_size, dset_batch_size= args.dset_batch_size, device=device)
     # read config file for model
+        if args.real_image_path and not os.path.exists(args.real_image_path):
+            os.makedirs(args.real_image_path)
         sample_images_real(data.val, args.image_count, args.real_image_path)
 
     if args.sample_gen:
